f11ad38e686929769452c2f643b489352e7550d9
SoChichung <ddeadwings@gmail.com>
2019-11-01 18:59:46 +0800

    2019-11-01

---
+++ data/change.js
@@ -0,0 +1,15 @@
+// json转成树形结构
+var data;
+var tree=[];
+data.forEach(element => {
+    let id=parseInt(element.categoryid);
+    if(tree[id]==undefined)
+    {
+        tree[id]={
+            id:id,
+            name:element.category_name,
+            children:[]
+        }
+    }
+    tree[id].children.push(element)
+});
