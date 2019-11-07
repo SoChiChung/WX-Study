48e30ac3b720a7154c2829b57d4b6b340cbb24df
SoChichung <ddeadwings@gmail.com>
2019-11-07 18:36:59 +0800

    git add./

--- pages/userinfo/userinfo.js
+++ pages/userinfo/userinfo.js
@@ -1,22 +1,31 @@
 // pages/userinfo/userinfo.js
 Page({
-    data: {
-        inputShowed: false,
-        inputVal: ""
-    },
-    onLoad() {
-        this.setData({
-            search: this.search.bind(this)
-        })
-    },
-    search: function (value) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}])
-            }, 200)
-        })
-    },
-    selectResult: function (e) {
-        console.log('select result', e.detail)
-    },
+  data: {
+    inputShowed: false,
+    inputVal: "",
+    switchchecked: true
+  },
+  onLoad() {
+    this.setData({
+      search: this.search.bind(this)
+    })
+  },
+  
+  /* 搜索 */
+  search: function(value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{
+          text: '搜索结果',
+          value: 1
+        }, {
+          text: '搜索结果2',
+          value: 2
+        }])
+      }, 200)
+    })
+  },
+  selectResult: function(e) {
+    console.log('select result', e.detail)
+  },
 });
