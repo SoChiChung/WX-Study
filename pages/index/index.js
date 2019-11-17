c396e89b315d7f1f20fdb260e8aa62863276b2f7
SoChichung <ddeadwings@gmail.com>
2019-11-17 11:44:33 +0800

    2019-11-17

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,20 +6,77 @@ const shuffle = require('./utils/shuffle.js')
 
 Page({
     data: {
-        headerTitleName: [
-            { name: '头条', nameID: '201701', newsType: 'top' },
-            { name: '军事', nameID: '201702', newsType: 'junshi' },
-            { name: '体育', nameID: '201703', newsType: 'tiyu' },
-            { name: '科技', nameID: '201704', newsType: 'keji' },
-            { name: '财经', nameID: '201705', newsType: 'caijing' },
-            { name: '社会', nameID: '201706', newsType: 'shehui' },
-            { name: '时尚', nameID: '201707', newsType: 'shishang' },
-            { name: '娱乐', nameID: '201708', newsType: 'yule' },
-            { name: '国内', nameID: '201709', newsType: 'guonei' },
-            { name: '国际', nameID: '2017010', newsType: 'guoji' }
+        height: 0,
+        widHeight: 0,
+        headerTitleName: [{
+                name: '头条',
+                nameID: '201701',
+                newsType: 'top'
+            },
+            {
+                name: '军事',
+                nameID: '201702',
+                newsType: 'junshi'
+            },
+            {
+                name: '体育',
+                nameID: '201703',
+                newsType: 'tiyu'
+            },
+            {
+                name: '科技',
+                nameID: '201704',
+                newsType: 'keji'
+            },
+            {
+                name: '财经',
+                nameID: '201705',
+                newsType: 'caijing'
+            },
+            {
+                name: '社会',
+                nameID: '201706',
+                newsType: 'shehui'
+            },
+            {
+                name: '时尚',
+                nameID: '201707',
+                newsType: 'shishang'
+            },
+            {
+                name: '娱乐',
+                nameID: '201708',
+                newsType: 'yule'
+            },
+            {
+                name: '国内',
+                nameID: '201709',
+                newsType: 'guonei'
+            },
+            {
+                name: '国际',
+                nameID: '2017010',
+                newsType: 'guoji'
+            }
         ],
         swiperIndex: '1/4',
-        topPic: [],
+        topPic: [{
+                imageUrl: "/images/banner/12001.png",
+                newsUrl: "https://www.wanplus.com/lol/video/1295093"
+            },
+            {
+                imageUrl: "/images/banner/120811.png",
+                newsUrl: "https://www.wanplus.com/article/263911.html"
+            },
+            {
+                imageUrl: "/images/banner/120809.png",
+                newsUrl: "https://www.wanplus.com/article/264003.html"
+            },
+            {
+                imageUrl: "/images/banner/120759.png",
+                newsUrl: "https://www.wanplus.com/article/263957.html"
+            }
+        ],
         tapID: 201701, // 判断是否选中
         contentNewsList: [],
         showCopyright: false,
@@ -27,7 +84,20 @@ Page({
         inputShowed: false,
         inputVal: "",
     },
-    onLoad: function () {
+    //根据图片设置swiper高度
+    setAutoSwiperHeight() {
+        var query = wx.createSelectorQuery();
+        var that = this;
+        query.select('.slide-image').boundingClientRect(function(rect) {
+
+            that.setData({
+                // 获取要循环标签的高度
+                height: rect.height,
+                widHeight: rect.height + "px"
+            })
+        }).exec();
+    },
+    onLoad: function() {
         this.setData({
             search: this.search.bind(this)
         })
@@ -36,26 +106,35 @@ Page({
                 showCopyright: true
             })
         })
+        /* this.setAutoSwiperHeight() */
     },
     //搜索
-    search: function (value) {
+    search: function(value) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+                resolve([{
+                    text: '搜索结果',
+                    value: 1
+                }, {
+                    text: '搜索结果2',
+                    value: 2
+                }])
             }, 200)
         })
     },
-    selectResult: function (e) {
+    selectResult: function(e) {
         console.log('select result', e.detail)
     },
     // headerBar 点击
-    headerTitleClick: function (e) {
-        this.setData({ tapID: e.target.dataset.id })
+    headerTitleClick: function(e) {
+        this.setData({
+            tapID: e.target.dataset.id
+        })
         this.renderPage(e.currentTarget.dataset.newstype, false)
     },
 
     //跳转到新闻详情页
-    viewDetail: function (e) {
+    viewDetail: function(e) {
         let newsUrl = e.currentTarget.dataset.newsurl || ''
         let newsTitle = e.currentTarget.dataset.newstitle || ''
         let newsAuthor = e.currentTarget.dataset.newsauthor || ''
@@ -64,7 +143,7 @@ Page({
         })
     },
 
-    handleSwiperChange: function (e) {
+    handleSwiperChange: function(e) {
         this.setData({
             swiperIndex: `${e.detail.current + 1}/4`
         })
@@ -78,15 +157,21 @@ Page({
         })
     },
     // isRefresh 是否为下拉刷新
-    renderPage: function (newsType, isRefresh, calllBack) {
+    renderPage: function(newsType, isRefresh, calllBack) {
         if (!isRefresh) {
             wx.showLoading({
                 title: '加载中'
             })
-            request({ url: `https://v.juhe.cn/toutiao/index?type=${newsType}&key=${appKey}`, newstype: newsType })
+            request({
+                    url: `https://v.juhe.cn/toutiao/index?type=${newsType}&key=${appKey}`,
+                    newstype: newsType
+                })
                 .then(res => {
                     wx.hideLoading()
-                    let { articleList, topPic } = extractArticleInfo(res.result.data)
+                    let {
+                        articleList,
+                        topPic
+                    } = extractArticleInfo(res.result.data)
                     this.setData({
                         contentNewsList: articleList,
                         /*             topPic */
@@ -101,11 +186,11 @@ Page({
                 request({
                     url: 'http://47.100.92.120:8080/image/carouse',
                     method: 'GET',
-                }).then(res=>{
-                    console.log('res',res)
-                    this.setData({
-                        topPic:res.data
-                    })
+                }).then(res => {
+                    console.log('res', res)
+                    /*                     this.setData({
+                                            topPic:res.data
+                                        }) */
                 })
         } else {
             // 数组随机排序，模拟刷新
